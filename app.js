baf18b52eb68e38f82274d636efc9ff8438ee49c
haroon <haroon@Dures-MacBook-Air.local>
2022-11-29 16:31:33 +0000

    Calculator working

---
+++ app.js
@@ -0,0 +1,73 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) =>{
+    event.preventDefault()
+    // Line below takes the form data from the HTML page and we have to make sure that 
+    // the name tags are filled in the HTML elements otherwise we will not get proper key pair values
+    // when the data is transformed
+    const linesData = new FormData(form);
+    // The linke below transforms the data into data with Key value pairs
+    const linesDataTransformed = Object.fromEntries(linesData);
+
+    // Picking out the SUVAT values from the transformed data 
+    const a = parseFloat(linesDataTransformed['a']);
+    const b = parseFloat(linesDataTransformed['b']);
+    const c = parseFloat(linesDataTransformed['c']);
+    const d = parseFloat(linesDataTransformed['d']);
+    const qmax = (a-c)/(2*b);
+    
+    
+    if ((a>0) ||(b>0) || (c>0)|| (d>=0) ) {
+        
+        document.querySelector(".errorhanger").innerText = ``
+        document.querySelector(".answer").innerText = `
+        The equations above are as follows:
+        Demand curve:
+        P = ${a} - ${b}Q
+        Cost function:
+        TC(Q) = ${c}Q+ ${d}
+
+        Profit = TR - TC
+        Profit = P*Q - (cQ+ d)
+        Profit = (${a} - ${b}Q)*Q - (${c}Q+ ${d})
+        Profit = ${a}Q - ${b}Q^2 - ${c}Q -${d}
+        Profit = ${a}Q - ${b}Q^2 - ${c}Q -${d}
+        Profit = -${b}Q^2 + ${a-c}Q -${d}
+
+        Differentiate with respect to Q, also call profit p
+
+        dp/dq = -2*${b}Q + ${a-c}
+
+        Set dp/dq = 0 and solve yeilds:
+
+        0 = -2*${b}Q + ${a-c}
+        2*${b}Q = ${a-c}
+        ${2*b}Q = $${a-c}
+        Q = ${(a-c)/(2*b)}
+
+        The price relating to this quantity is:
+
+        P = ${a} - ${b}Q
+        P = ${a} - ${b*qmax}
+        P = ${a - (b*qmax) }
+        
+
+        The profit relating to this quantity is:
+
+        Profit = -${b}Q^2 + ${a-c}Q -${d}
+        Profit = -${b}${qmax}^2 + ${a-c}${qmax} -${d}
+        Profit = ${(-1)*b}${qmax*qmax} + ${(a-c)*qmax} -${d}
+        Profit = ${((-1)*b) *(qmax*qmax)} + ${ ((a-c)*qmax) - d} 
+        Profit = ${  (((-1)*b) *(qmax*qmax)) + (((a-c)*qmax) - d) }
+
+        `
+ 
+    } else {
+
+        document.querySelector(".errorhanger").innerText = `Please insert appropiate values for a, b, c and d`       
+
+    }
+
+
+}
+)
